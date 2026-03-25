1f1e489d86049b9f5c0f7c25168fc63444f5b0c4
Lucas Filipe Cordova <lucascordova1000@gmail.com>
2026-03-25 10:13:28 -0300

    chore: lógica inicial definida, criação das funções CRUD, testes iniciais com localforage

---
+++ src/storage/authorStorage.ts
@@ -0,0 +1,31 @@
+import { db } from './index.ts';
+import type {Author} from '../types/author.ts';
+
+const KEY = 'authors';
+
+export const getAuthors = async (): Promise<Author[]> => {
+    const authors = await db.getItem<Author[]>(KEY);
+    return authors || [];
+};
+
+export const saveAuthors = async (authors: Author[]) => {
+    await db.setItem(KEY, authors);
+};
+
+export const createAuthor = async (author: Author) => {
+    const authors = await getAuthors();
+    const updated = [...authors, author];
+    await saveAuthors(updated);
+    return author;
+};
+
+export const getAuthorById = async (id: string) => {
+    const authors = await getAuthors();
+    return authors.find(a => a.id === id) || null;
+};
+
+export const deleteAuthor = async (id: string) => {
+    const authors = await getAuthors();
+    const updated = authors.filter(a => a.id !== id);
+    await saveAuthors(updated);
+};
