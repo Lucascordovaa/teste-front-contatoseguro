246736dd8dc10c4d3a32c8bd6961f42f430e4f9a
Lucas Filipe Cordova <lucascordova1000@gmail.com>
2026-03-25 10:58:49 -0300

    feature: página autores funcional com os requisitos de CRUD

--- src/pages/AuthorsPage.tsx
+++ src/pages/AuthorsPage.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
-import { Table, Button } from 'antd';
+import {Table, Button, Modal} from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import type { Author } from '../types/author';
 import { getAuthors } from '../storage/authorStorage';
 import CreateAuthorModal from '../components/CreateAuthorModal';
+import { deleteAuthor } from '../storage/authorStorage';
+import { Popconfirm } from 'antd';
 
 function AuthorsPage() {
     const [authors, setAuthors] = useState<Author[]>([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedAuthor, setSelectedAuthor] = useState<Author | null>(null);
 
     const loadAuthors = async () => {
         const data = await getAuthors();
@@ -18,6 +21,11 @@ function AuthorsPage() {
         loadAuthors();
     }, []);
 
+    const handleDelete = async (id: string) => {
+        await deleteAuthor(id);
+        loadAuthors();
+    };
+
     const columns: ColumnsType<Author> = [
         {
             title: 'Name',
@@ -26,6 +34,29 @@ function AuthorsPage() {
         {
             title: 'Email',
             dataIndex: 'email',
+        },
+        {
+            title: 'Actions',
+            render: (_, record) => (
+                <>
+                    <Button
+                        style={{ marginRight: 8 }}
+                        onClick={() => setSelectedAuthor(record)}
+                    >
+                        View
+                    </Button>
+
+                    <Popconfirm
+                        title="Delete this author?"
+                        description="Are you sure you want to delete this author?"
+                        onConfirm={() => handleDelete(record.id)}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <Button danger>Delete</Button>
+                    </Popconfirm>
+                </>
+            ),
         },
     ];
 
@@ -53,6 +84,16 @@ function AuthorsPage() {
                 onClose={() => setIsModalOpen(false)}
                 onSuccess={loadAuthors}
             />
+
+            <Modal
+                title="Author Details"
+                open={!!selectedAuthor}
+                onCancel={() => setSelectedAuthor(null)}
+                footer={null}
+            >
+                <p><strong>Name:</strong> {selectedAuthor?.name}</p>
+                <p><strong>Email:</strong> {selectedAuthor?.email || '—'}</p>
+            </Modal>
         </div>
     );
 }
